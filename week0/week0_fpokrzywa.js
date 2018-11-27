63d2f8c17372201303c8aacacfb0077dc2f65c73
fpokrzywa <freddie@3cpublish.com>
2018-11-26 21:32:53 -0500

    Update week0_fpokrzywa.js

--- week0/week0_fpokrzywa.js
+++ week0/week0_fpokrzywa.js
@@ -1,8 +1,4 @@
-/*
-Week 0 Assignments for Blockgeeks
-*/
-
-//Task 1: Repeating Numbers
+console.log("\n********** Task 1: Repeating Numbers **********");
 var repeatNumbers = function(data) {
   var result = '';
   
@@ -21,18 +17,11 @@ var repeatNumbers = function(data) {
   
 };
 
-//console.log(repeatNumbers([[1, 10]]));
-//console.log(repeatNumbers([[1, 2], [2, 3]]));
-//console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
 
-/*
-Expected Output
-1111111111
-11, 222
-10101010, 343434343434, 9292
-*/
-
-//Task 2: Conditional Sums
+console.log("\n********** Task 2: Conditional Sums ********** ");
 var conditionalSum = function(values, condition) {
 	var data = 0;
 
@@ -49,20 +38,12 @@ var conditionalSum = function(values, condition) {
   
 };
 
-//console.log(conditionalSum([1, 2, 3, 4, 5], "Even"));
-//console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
-//console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
-//console.log(conditionalSum([], "odd"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "Even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
 
-/*
-Expected Output
-6
-9
-144
-0
-*/
-
-//Task 3: Talking Calendar
+console.log("\n********** Task 3: Talking Calendar ********** ");
 var talkingCalendar = function(date) {
 	var cal = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 	var d = '';
@@ -80,32 +61,31 @@ var talkingCalendar = function(date) {
 	return "No proper date format";
 };
 
-//console.log(talkingCalendar("2017/12/02"));
-//console.log(talkingCalendar("2007/11/11"));
-//console.log(talkingCalendar("1987/08/24"));
-/*
-Expected Output
-December 2nd, 2017
-November 11th, 2007
-August 24th, 1987
-*/
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
 
-//Task 4: Change Calculator
+console.log("\n********** Task 4: Change Calculator **********");
 var calculateChange = function(total, cash) {
 	var balance = cash - total;
-	var b = (balance);
+	var change = {};
+	var amount;
+    var coinTypes = [2000, 1000, 500, 200, 100, 25, 10, 5, 1];
+    var denominations = ["twenty", "ten", "five", "twoDollar", "dollar", "quarter", "dime", "nickel", "penny"];
+	for(i in coinTypes){
+		amount = Math.floor(balance/coinTypes[i]);
+		if(amount > 0){
+			change[denominations[i]] = amount;
+			balance = balance%coinTypes[i];
+		}
+
+	}
 	
-	return b.toFixed(2);
+	return change;
 };
 
 console.log(calculateChange(1787, 2000));
-//console.log(calculateChange(2623, 4000));
-//console.log(calculateChange(501, 1000));
-/*
-Expected Output
-{ twoDollar: 1, dime: 1, penny: 3 }
-{ ten: 1, twoDollar: 1, dollar: 1, quarter: 3, penny: 2 }
-{ twoDollar: 2, quarter: 3, dime: 2, penny: 4 }
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
 
-*/
 
